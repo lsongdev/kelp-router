780301c24f3c57ab596e8a79e0b4f6ac51e6ed86
Lsong <song940@gmail.com>
2016-03-05 19:44:36 +0800

    add multiple middlewares support .

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 'use strict';
-const url = require('url');
+const url          = require('url');
 const pathToRegexp = require('path-to-regexp');
-
-function route(method, path, callback){
-  if(arguments.length == 2){
-    callback = path;
-    path = method;
-    method = null;
-  }
-  var keys = [];
-  var regexp = pathToRegexp(path, keys);
+/**
+ * [route description]
+ * @param  {[type]} method      [description]
+ * @param  {[type]} path        [description]
+ * @param  {[type]} middlewares [description]
+ * @return {[type]}             [description]
+ */
+module.exports = function route(method, path, middlewares){
+  var offset = (typeof arguments[1] == 'string') ? 0 : -1;
+  method      = arguments[ offset ];
+  path        = arguments[ offset + 1 ];
+  middlewares = [].slice.call(arguments, offset + 2);
+  middlewares = [].concat.apply([], middlewares);
+  var keys = [], regexp = pathToRegexp(path, keys);
   return function(req, res, next){
-    var u = url.parse(req.url, true);
-    req.params = {};
-    req.query = u.query;
-    req.path = u.pathname;
-    if((method ? (method.toUpperCase() == req.method) : true) &&  regexp.test(req.path)){
-      var matchs = regexp.exec(req.path).slice(1);
-      keys.forEach(function(key, i){
-        req.params[ key.name ] = matchs[ i ];
+    var path = decodeURIComponent(url.parse(req.url).pathname), self = this, i = -1;
+    if((method ? (method.toUpperCase() == req.method) : true) &&  regexp.test(path)){
+      req.params  = {};
+      var matchs = regexp.exec(path).slice(1);
+      keys.forEach(function(key, index){
+        req.params[ key.name ] = matchs[ index ];
       });
-      callback.apply(this, arguments);
+      (function fn(err){
+        var middleware = middlewares[ ++i ];
+        if(typeof middleware == 'function'){
+          middleware.apply(self, [ req, res, fn, err ]);
+        }
+      })();
     }else{
       next();
     }
   }
 };
-
-route.get = function(path, callback){
-  return route('get', path, callback);
-};
-
-module.exports = route;
